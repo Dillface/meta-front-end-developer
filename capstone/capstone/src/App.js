d64481ee8bd7d0e29b7f4f3429da6fc03cd810ce
Dillface <dylan_maclennan@yahoo.co.uk>
2025-07-10 11:29:56 +0100

    init files

---
+++ capstone/capstone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Header, Footer, Nav} from './Components';
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Nav/>
+   <main></main>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
